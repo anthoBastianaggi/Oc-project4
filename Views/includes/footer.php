55f3ba6c7f18d76dc9c710d3d76c4251b801a683
anthoBastianaggi <a.bastianaggi@gmail.com>
2020-01-09 23:07:52 +0100

    [Refactor-Footer] (Update) Refactor structure footer

--- Views/includes/footer.php
+++ Views/includes/footer.php
@@ -1,5 +1,61 @@
-<div class="footer-copyright text-center">© 2018 Copyright:
-    <a href="<?= CURRENT_PATH ?>home"> Jean Forteroche - Tous droits réservés.</a>
-    <br />
-    <p class="footer-info text-center">Site développé dans le cadre d'une formation Openclassrooms.</p>
+<div class="containerSection">
+    <section id="footer"> 
+        <div class="container"> 
+            <div class="row"> 
+                <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5 socialContainer"> 
+                    <ul class="list-unstyled list-inline social text-center"> 
+                        <li class="list-inline-item">
+                            <a href="javascript:void();">
+                                <i class="fa fa-facebook">
+                                    <span class="d-none d-lg-inline-flex">Facebook</span>
+                                </i>
+                            </a>
+                        </li> 
+                        <li class="list-inline-item">
+                            <a href="javascript:void();">
+                                <i class="fa fa-twitter">
+                                    <span class="d-none d-lg-inline-flex">Twitter</span>
+                                </i>
+                            </a>
+                        </li> 
+                        <li class="list-inline-item">
+                            <a href="javascript:void();">
+                                <i class="fa fa-instagram">
+                                    <span class="d-none d-lg-inline-flex">Instagram</span>
+                                </i>
+                            </a>
+                        </li> 
+                        <li class="list-inline-item">
+                            <a href="javascript:void();">
+                                <i class="fa fa-snapchat">
+                                    <span class="d-none d-lg-inline-flex">Snapchat</span>
+                                </i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="javascript:void();" target="_blank">
+                                <i class="fa fa-linkedin">
+                                    <span class="d-none d-lg-inline-flex">Linkedin</span>
+                                </i>
+                            </a>
+                        </li> 
+                    </ul> 
+                </div> 
+                </hr> 
+            </div>     
+        </div> 
+    </section> 
+    <div class="separator"></div>
+    <section id="copyright"> 
+        <div class="copyrightContainer"> 
+            <div class="row"> 
+                <div class="footer-copyright text-center">© 2018 Copyright:
+                    <a href="<?= CURRENT_PATH ?>home"> Jean Forteroche - Tous droits réservés.</a>
+                    <br />
+                    <p class="footer-info text-center">Site développé dans le cadre d'une formation Openclassrooms.</p>
+                </div>
+            </div>
+        </div> 
+    </section>
 </div>
+
