f3e1482cb7e9d1868ab135fff62afd43e9fd3621
anthoBastianaggi <a.bastianaggi@gmail.com>
2020-01-18 04:16:39 +0100

    [Refactor-Connexion] (Update) Refactor login form

--- Views/Account/Sections/Log/Login/login.php
+++ Views/Account/Sections/Log/Login/login.php
@@ -8,30 +8,57 @@
 <section id="login" class="content-section text-left">
     <div class="container">
         <div class="row">
-            <div class="col-md-12 col-md-4">
+            <div class="col-md-12 col-md-5 textHeaderLogin">
                 <div class="text-container">
-                    <h2 class="section-title text-center text-uppercase">Se connecter</h2>
+                    <h2 class="section-title text-center text-uppercase">Se connecter au compte</h2>                   
                 </div>
             </div>
-            <div  id="infobox-login" class="col-md-12 col-md-5">
-                <div class="box-shadow-full">
-                    <div class="row">
+            <div  id="infobox-login" class="col-sm-12">
+                <div class="box-shadow-full boxContainer">
+                    <div class="row">   
+                        <div class="infoForgetContainer">
+                            <div class="iconContainer">
+                                <span class="icon-case"><i class="fa fa-users"></i></span>
+                            </div>
+                            <div class="more-info description">
+                                <p class="lead">
+                                  Connexion membres
+                                </p>                           
+                            </div>
+                        </div>       
                         <div class="col-md-12 col-md-offset-2">
-                            <form action="" method="POST">
+                            <form class="form-horizontal" action="" method="POST">                        
                                 <div class="form-group">
-                                    <label class="label-login" for="">Pseudo ou email</label>
-                                    <input type="text" name="username" maxlength="25" class="form-control" id="exampleInputName2" placeholder="Entrez votre pseudo ou email"/>
-                                </div>
+                                    <div class="inputGroupContainer">
+                                        <div class="input-group">
+                                            <label class="label-login" for="">Nom d'utilisateur ou adresse e-mail</label>
+                                            <span class="icon-case"><i class="fa fa-user"></i></span>
+                                            <input type="text" name="username" maxlength="25" class="form-control" id="exampleInputName2" placeholder="Entrez votre nom d'utilisateur ou adresse e-mail"/>                                                                                                               
+                                        </div>
+                                    </div>
+                                </div>   
                                 <div class="form-group">
-                                    <label class="label-login" for="">Mot de passe <br /><a href="<?= CURRENT_PATH ?>forget?action=forget">(J'ai oublié mon mot de passe)</a></label>
-                                    <input type="password" name="password" minlength="8" maxlength="32" class="form-control" id="exampleInputName2" placeholder="Entrez votre mot de passe" />
-                                </div>
+                                    <div class="inputGroupContainer">
+                                        <div class="input-group">
+                                            <label class="label-login" for="">Mot de passe</label>
+                                            <span class="icon-case"><i class="fa fa-lock"></i></span> 
+                                            <input type="password" name="password" minlength="8" maxlength="32" class="form-control" id="exampleInputName2" placeholder="Entrez votre mot de passe" />                                                                    
+                                        </div>
+                                    </div>
+                                </div>     
                                 <div class="form-group">
-                                    <label class="label-login">
-                                        <input type="checkbox" name="remember" value="1"/> Se souvenir de moi 
-                                    </label>
-                                </div>
-                                <button type="submit" class="btn btn-primary" id="btn-login">Se connecter</button>
+                                    <div class="inputGroupContainer">
+                                        <div class="input-group">
+                                            <a href="<?= CURRENT_PATH ?>forget?action=forget">(J'ai oublié mon mot de passe)</a>
+                                            <label class="label-login">
+                                                <input type="checkbox" name="remember" value="1"/> Se souvenir de moi 
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>                    
+                                <div class="col-md-12 text-center" id="btn-login">
+                                    <button type="submit" class="btn btn-primary">Connexion</button>
+                                </div>                           
                             </form>
                         </div>
                     </div>
@@ -40,7 +67,6 @@
         </div>
     </div>
 </section>
-
 <?php 
     // On recupere dans une variable le contenu du tampon 
     $content = ob_get_clean() ;
