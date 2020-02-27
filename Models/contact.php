7378e2bf6edb21480598301d75f8eccb8dbd34db
a.bastianaggi <a.bastianaggi@gmail.com>
2020-02-27 09:11:27 +0100

    [Refactor-Contact] (Add) Add fonctionality is logout

--- Models/contact.php
+++ Models/contact.php
@@ -13,10 +13,17 @@ class Contact {
         * Ajout d'un message de contact
         * @return array
     */
-    public function addMessageContact($authorContact, $subject, $content) {
+    public function addMessageContactIsConnected($authorContact, $subject, $content) {
         global $db;
 
         $reqContact= $db->prepare('INSERT INTO message(users_id, subject, content, sended_at) VALUES(?, ?, ?, NOW())');
         $reqContact->execute(array($authorContact, $subject, $content));
+    }
+
+    public function addMessageContact($firstnameContact, $lastnameConctact, $emailContact, $subject, $content) {
+        global $db;
+
+        $reqContact= $db->prepare('INSERT INTO message(firstname, lastname, email, subject, content, sended_at) VALUES(?, ?, ?, ?, ?, NOW())');
+        $reqContact->execute(array($firstnameContact,$lastnameConctact, $emailContact, $subject, $content));
     }
 }
