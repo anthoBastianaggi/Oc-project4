eefb00e7bd5ca19ce530c807d6c5e6aa13931864
anthoBastianaggi <a.bastianaggi@gmail.com>
2020-04-11 15:53:38 +0200

    [master] (Fix) fix bug submit update-email

--- controllers/update-email.php
+++ controllers/update-email.php
@@ -7,17 +7,14 @@ function updateEmail($page) {
             $db = App::getDatabase();
             $validator = new Validator($_POST);
             $validator->isEmail('email', "Votre email n'est pas valide.");
-            if($validator->isValid()) {
-                $validator->isUniq('email', $db, 'users', "Cet email est déjà utilisé pour un autre compte.");
-            }
             if($_POST['email'] !== $_SESSION['auth']->email) {
                 Session::getInstance()->setFlash('danger', "Erreur l'adresse rentrée est différente de l'adresse actuelle. Veuillez rentrer la bonne adresse.");
             }
             if(!empty($_POST['new-email'])) { 
                 if($_POST['new-email'] === $_POST['email']) {
                     Session::getInstance()->setFlash('danger', "Erreur. Veuillez rentrer une adresse différente que celle actuelle.");
-                } 
-                if($validator->isValid()){
+                } else if($validator->isValid()){
+                    $validator->isUniq('email', $db, 'users', "Cet email est déjà utilisé pour un autre compte.");
                     $updateEmail = new Settings();
                     $updateEmail->updateEmail($_POST['new-email'], $_SESSION['auth']->id);
                     Session::getInstance()->setFlash('success', "Votre adresse email a bien été modifiée.");
